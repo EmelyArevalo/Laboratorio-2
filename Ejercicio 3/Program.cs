ba3f00af1f25faa3cb94a68a1f9b86d8c66ac03d
Michelle Arèvalo <emelymichellearlo@gmail.com>
2022-08-15 23:55:00 -0600

    Cambios

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el número a procesar: ");
+            int multiplo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el límite: ");
+            int limite = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= limite; i++)
+            {
+                if (i %multiplo == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+        }
+    }
+}
---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+            int cont = 0;
+
+            Console.WriteLine("Ingrese el número de repeticiones que desea: ");
+            int limite = int.Parse(Console.ReadLine());
+
+            while (condition)
+            {
+                if (cont == limite)
+                    condition = false;
+
+                Console.WriteLine(cont);
+                cont++;
+
+
+
+
+
+        }   }
+    }
+}
